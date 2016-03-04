f9be49c55c3bbece414e75932eb46b4ff23bbfc4
Marcelo Vani <marcellovani@yahoo.co.uk>
2016-03-04 01:40:09 +0000

    Added via @twitteruser. Renamed settings

--- src/Form/AdminSettingsForm.php
+++ src/Form/AdminSettingsForm.php
@@ -9,7 +9,7 @@ namespace Drupal\sharerich\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
-
+use Drupal\Component\Utility\Xss;
 /**
  * Class AdminSettingsForm.
  *
@@ -39,35 +39,54 @@ class AdminSettingsForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('sharerich.settings');
 
+    $form['global'] = array(
+      '#type' => 'fieldset',
+      '#title' => $this->t('Global settings'),
+      '#collapsible' => FALSE,
+      '#collapsed' => FALSE,
+    );
+    $form['global']['allowed_html'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Allowed HTML tags'),
+      '#description' => $this->t('A list of HTML tags that can be used'),
+      '#default_value' => $config->get('allowed_html'),
+    );
+
     $form['social'] = array(
       '#type' => 'fieldset',
       '#title' => $this->t('Social networks'),
       '#collapsible' => TRUE,
       '#collapsed' => FALSE,
     );
-    $form['social']['sharerich_facebook_app_id'] = array(
+    $form['social']['facebook_app_id'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Facebook App ID'),
       '#description' => $this->t('You need to have an App ID, which you can get from Facebook.'),
-      '#default_value' => $config->get('sharerich_facebook_app_id'),
+      '#default_value' => $config->get('facebook_app_id'),
     );
-    $form['social']['sharerich_facebook_site_url'] = array(
+    $form['social']['facebook_site_url'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Facebook Site URL'),
       '#description' => $this->t('You need to have an App ID, which you can get from Facebook.'),
-      '#default_value' => $config->get('sharerich_facebook_site_url'),
+      '#default_value' => $config->get('facebook_site_url'),
     );
-    $form['social']['sharerich_youtube_username'] = array(
+    $form['social']['youtube_username'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('YouTube Username'),
       '#description' => $this->t('Enter your YouTube username in order for the social button to link to your YouTube channel.'),
-      '#default_value' => $config->get('sharerich_youtube_username'),
+      '#default_value' => $config->get('youtube_username'),
     );
-    $form['social']['sharerich_github_username'] = array(
+    $form['social']['github_username'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Github Username'),
       '#description' => $this->t('Enter your Github username in order for the social button to link to your Github profile.'),
-      '#default_value' => $config->get('sharerich_github_username'),
+      '#default_value' => $config->get('github_username'),
+    );
+    $form['social']['twitter_user'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Twitter user'),
+      '#description' => $this->t('Used when sharing on twitter to identify the person sharing i.e. via @userid.'),
+      '#default_value' => $config->get('twitter_user'),
     );
 
     return parent::buildForm($form, $form_state);
@@ -87,10 +106,12 @@ class AdminSettingsForm extends ConfigFormBase {
     parent::submitForm($form, $form_state);
 
     $this->config('sharerich.settings')
-      ->set('sharerich_facebook_app_id', $form_state->getValue('sharerich_facebook_app_id'))
-      ->set('sharerich_facebook_site_url', $form_state->getValue('sharerich_facebook_site_url'))
-      ->set('sharerich_youtube_username', $form_state->getValue('sharerich_youtube_username'))
-      ->set('sharerich_github_username', $form_state->getValue('sharerich_github_username'))
+      ->set('allowed_html', $form_state->getValue('allowed_html'))
+      ->set('facebook_app_id', $form_state->getValue('facebook_app_id'))
+      ->set('facebook_site_url', $form_state->getValue('facebook_site_url'))
+      ->set('youtube_username', $form_state->getValue('youtube_username'))
+      ->set('github_username', $form_state->getValue('github_username'))
+      ->set('twitter_user', $form_state->getValue('twitter_user'))
       ->save();
   }
 
